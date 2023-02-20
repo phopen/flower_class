e983ff6bd746827887c1baf4e02ca42a3746a84a
phopen <phopen@umass.edu>
2023-02-20 15:48:21 -0500

    Read flower data, implement resizing

--- utils.py
+++ utils.py
@@ -5,20 +5,13 @@ import matplotlib.pyplot as plt
 import scipy.io as spio
 from skimage.feature import plot_matches
 from skimage.util import montage
-
-# images (x) is the array e.g., data['x']
-def montageDigits(x):
-    num_images = x.shape[2]
-    m = montage(x.transpose(2, 0, 1))
-    plt.imshow(m, cmap='gray')
-    plt.show()
-
-    return np.mean(x, axis=2)
+from skimage.io import imread_collection
+from skimage.io import concatenate_images
+import cv2
 
 
-# Evaluate the accuracy of labels
+
 def evaluateLabels(y, ypred, visualize=True):
-
     classLabels = np.unique(y)
     conf = np.zeros((len(classLabels), len(classLabels)))
     for tc in range(len(classLabels)):
@@ -64,54 +57,47 @@ def todict(matobj):
             dict[strg] = elem
     return dict
 
-#data.mat is a dict with the following structure:
-#   {train:
-#       {x: array with raw data (image height, image width, number of images)}
-#       {y: array with corresponding labels}},
-#   {test:
-#       {x: array with raw data (image height, image width, number of images)}
-#       {y: array with corresponding labels}}
-def loadmat(path):
-    return todict(spio.loadmat(path,
-                               struct_as_record=False,
-                               squeeze_me=True)['data'])
+def load_images(folder):
+    images = imread_collection(folder)
+    images_arr = concatenate_images(images)
+    return images_arr
 
 
-def imread(path):
-    img = plt.imread(path).astype(float)
-    #Remove alpha channel if it exists
-    if img.ndim > 2 and img.shape[2] == 4:
-        img = img[:, :, 0:3]
-    #Puts images values in range [0,1]
-    if img.max() > 1.0:
-        img /= 255.0
+# def imread(path):
+#     img = plt.imread(path).astype(float)
+#     #Remove alpha channel if it exists
+#     if img.ndim > 2 and img.shape[2] == 4:
+#         img = img[:, :, 0:3]
+#     #Puts images values in range [0,1]
+#     if img.max() > 1.0:
+#         img /= 255.0
+#
+#     return img
 
-    return img
 
-
-def mkdir(dirpath):
-    if not os.path.exists(dirpath):
-        try:
-            os.makedirs(directory)
-        except OSError as e:
-            if e.errno != errno.EEXIST:
-                raise
-    else:
-        print("Directory {} already exists.".format(dirpath))
+# def mkdir(dirpath):
+#     if not os.path.exists(dirpath):
+#         try:
+#             os.makedirs(directory)
+#         except OSError as e:
+#             if e.errno != errno.EEXIST:
+#                 raise
+#     else:
+#         print("Directory {} already exists.".format(dirpath))
 
 
 # Thanks to ali_m from https://stackoverflow.com/questions/17190649
-def gaussian(hsize=3,sigma=0.5):
-    """
-    2D gaussian mask - should give the same result as MATLAB's
-    fspecial('gaussian',[shape],[sigma])
-    """
-    shape = (hsize, hsize)
-    m,n = [(ss-1.)/2. for ss in shape]
-    y,x = np.ogrid[-m:m+1,-n:n+1]
-    h = np.exp( -(x*x + y*y) / (2.*sigma*sigma) )
-    h[ h < np.finfo(h.dtype).eps*h.max() ] = 0
-    sumh = h.sum()
-    if sumh != 0:
-        h /= sumh
-    return h
+# def gaussian(hsize=3,sigma=0.5):
+#     """
+#     2D gaussian mask - should give the same result as MATLAB's
+#     fspecial('gaussian',[shape],[sigma])
+#     """
+#     shape = (hsize, hsize)
+#     m,n = [(ss-1.)/2. for ss in shape]
+#     y,x = np.ogrid[-m:m+1,-n:n+1]
+#     h = np.exp( -(x*x + y*y) / (2.*sigma*sigma) )
+#     h[ h < np.finfo(h.dtype).eps*h.max() ] = 0
+#     sumh = h.sum()
+#     if sumh != 0:
+#         h /= sumh
+#     return h
